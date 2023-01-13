a1235947efa6fa7c0435cc4e198e46fb30da83ce
Asquare-B <michal.homes11@gmail.com>
2023-01-13 16:46:17 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './myComponents/homepage/homepage.component';
+import { HeaderComponent } from './myComponents/header/header.component';
+import { FooterComponent } from './myComponents/footer/footer.component';
+import { NavbarComponent } from './myComponents/navbar/navbar.component';
+import { SearchbarComponent } from './myComponents/searchbar/searchbar.component';
+import { HomeOffersComponent } from './myComponents/home-offers/home-offers.component';
+import { OffercardComponent } from './myComponents/offercard/offercard.component';
+import { FeaturesComponent } from './myComponents/features/features.component';
+import { FeatureCardComponent } from './myComponents/feature-card/feature-card.component';
+import { ShopComponent } from './myComponents/shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    SearchbarComponent,
+    HomeOffersComponent,
+    OffercardComponent,
+    FeaturesComponent,
+    FeatureCardComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,

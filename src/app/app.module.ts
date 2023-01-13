6ff668a7ffde02c2d2ac80af53337068c4c160cb
Asquare-B <michal.homes11@gmail.com>
2023-01-13 20:53:36 +0530

    shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import  {NgxPaginationModule } from 'ngx-pagination';
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './myComponents/homepage/homepage.component';
@@ -13,11 +16,21 @@ import { OffercardComponent } from './myComponents/offercard/offercard.component
 import { FeaturesComponent } from './myComponents/features/features.component';
 import { FeatureCardComponent } from './myComponents/feature-card/feature-card.component';
 import { ShopComponent } from './myComponents/shop/shop.component';
+import { ShopMainComponent } from './myComponents/shop-main/shop-main.component';
+import { ShopCardComponent } from './myComponents/shop-card/shop-card.component';
+import { ShopHeadComponent } from './myComponents/shop-head/shop-head.component';
+import { ShopProductComponent } from './myComponents/shop-product/shop-product.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CartComponent } from './myComponents/cart/cart.component';
+import { CartItemComponent } from './myComponents/cart-item/cart-item.component';
+import { PdhomeComponent } from './myComponents/pdhome/pdhome.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
+    ShopHeadComponent,
     HeaderComponent,
     FooterComponent,
     NavbarComponent,
@@ -26,11 +39,19 @@ import { ShopComponent } from './myComponents/shop/shop.component';
     OffercardComponent,
     FeaturesComponent,
     FeatureCardComponent,
-    ShopComponent
+    ShopComponent,
+    ShopMainComponent,
+    ShopCardComponent,
+    ShopProductComponent,
+    CartComponent,
+    CartItemComponent,
+    PdhomeComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
